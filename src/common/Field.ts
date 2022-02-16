79955ce99ad259bbdfebb55c0985c7c1627d1ba4
teionhozonn <teionhozonn@gmail.com>
2022-02-16 18:23:34 +0900

    update Field.ts

--- src/common/Field.ts
+++ src/common/Field.ts
@@ -48,4 +48,35 @@ export class Field {
 
         return new Field(newFieldData);
     }
+
+    canMove = (data: number[][], position: { x: number, y: number }): boolean => {
+        const y_max = this.field.length;
+        const x_max = this.field[0].length;
+
+        if (position.y >= y_max) return false;
+
+        for (let i = 0; i < data.length; i++) {
+            const rows = data[i];
+            for (let j = 0; j < rows.length; j++) {
+                const block = rows[j];
+                // テトリミノが描画される予定のマス目 (block > 0) であれば、そのマス目が移動可能なマス目かどうかをチェックする
+                if (block > 0) {
+                    // テトリミノのマス目が、テトリスのフィールドの下にはみ出していないか判定する
+                    if (i + position.y > y_max - 1 ||
+                        // テトリミノのマス目が、テトリスのフィールドの左にはみ出していないか判定する
+                        0 > j + position.x ||
+                        // テトリミノのマス目が、テトリスのフィールドの右にはみ出していないか判定する
+                        j + position.x > x_max - 1 ||
+                        // テトリミノのマス目に、他のテトリミノが既に配置されているかどうか判定する
+                        this.field[i + position.y][j + position.x] > 0) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+
+
 }
